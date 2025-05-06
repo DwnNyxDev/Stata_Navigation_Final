d69581a223a0af73492326f16fe75ada346fe496
DwnNyxDev <vatelvandell@gmail.com>
2025-05-06 12:25:22 -0400

    Updated to work with update to model saving design

--- test_model.py
+++ test_model.py
@@ -3,37 +3,28 @@ import torch
 from torchvision import models, transforms
 from PIL import Image
 import urllib
-import os
-import dropbox
 
 # Initialize the model
 model = models.resnet50(num_classes=5)
-model.fc = torch.nn.Sequential(
-    torch.nn.Dropout(p=0.3),  # Add dropout for regularization
-    torch.nn.Linear(model.fc.in_features, 5)  # Replace the final layer to match the number of classes
-)
 
-#If GPU is available, load the model to GPU
-if torch.cuda.is_available():
-    model = model.to('cuda')
-else:
-    model = model.to('cpu')
-
-
-model.eval()  # Set the model to evaluation mode
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Load the model weights
 model_root = "models"
-model_name = "yw0"
-model_epoch = 8
+model_name = "Ywl"
 
-model_path = f"{model_root}/{model_name}/epoch_{model_epoch}.pth"
+model_path = f"{model_root}/{model_name}.pth"
 state = torch.load(model_path, map_location='cpu')
-model.load_state_dict(state['model_state_dict'])
 
 classes = state['class_names']
-print(classes)
+model.fc = torch.nn.Sequential(
+    torch.nn.Dropout(p=0.3),  # Add dropout for regularization
+    torch.nn.Linear(model.fc.in_features, len(classes))  # Replace the final layer to match the number of classes
+)
 
+model.load_state_dict(state['model_state_dict'])
+model = model.to(device)
+model.eval()  # Set the model to evaluation mode
 
 # Define an image preprocessing function
 # The model expects images of size 224x224 and normalized with ImageNet statistics
@@ -50,7 +41,7 @@ transform = transforms.Compose([
 # Load an image from a URL image address
 
 # r&d pub
-image_url = "https://www.dropbox.com/scl/fi/31du68lvjyrqotjxvpyoq/20250426_160655.jpg?rlkey=m8pq24avrn72b6ncil9cbaza2&st=vq5sbkqg&raw=1"
+image_url = "https://www.dropbox.com/scl/fi/0dn4c40ppt1l270mq9lsb/20250426_160700.jpg?rlkey=n4mgs5vdjjib14itb0ihwumvq&st=7nnrkmzn&raw=1"
 
 # patio
 # image_url = "https://www.dropbox.com/scl/fi/pktsbgdvh42a9wvoidtsp/20250426_161320-0.jpg?rlkey=12oos5fbn4qkvmcnk29uhgjgu&st=1u6glbd4&raw=1"
@@ -63,6 +54,9 @@ image_url = "https://www.dropbox.com/scl/fi/31du68lvjyrqotjxvpyoq/20250426_16065
 
 #stata
 # image_url = "https://www.dropbox.com/scl/fi/nl0r88rws0idit3bejrg3/20250426_155636-1.jpg?rlkey=09c84u1gcgealygw1kg45z0th&st=p6on4q2q&raw=1"
+
+#cafeteria
+# image_url = "https://www.dropbox.com/scl/fi/9nfak80ztiu7t55pwp6zo/20250426_162809.jpg?rlkey=w4lwpou74qkd61bi6vstj0d13&st=sesmao71&raw=1"
 img = Image.open(urllib.request.urlopen(image_url)).convert('RGB')
 
 input_img = transform(img).unsqueeze(0)
@@ -70,6 +64,7 @@ input_img = transform(img).unsqueeze(0)
 #Run the model on the input image
 # Forward pass
 with torch.no_grad():
+    input_img = input_img.to('cuda') if torch.cuda.is_available() else input_img.to('cpu')
     logits = model(input_img)
     probs = torch.nn.functional.softmax(logits, dim=1)
 
